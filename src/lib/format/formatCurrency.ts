470321915ccf8c46564828d9577088de38f153a7
caligo-son <juhyoung.son@pearlblackfs.com>
2021-05-06 17:15:42 +0900

    currency format discard code add

--- src/lib/format/formatCurrency.ts
+++ src/lib/format/formatCurrency.ts
@@ -1,7 +1,8 @@
 import numbro from 'numbro';
 
 export const formatCurrency = (value, decimals = 2) => {
-	if (!value) return 0;
-	if (!Number(value)) return 0;
-	return numbro(value).format( {thousandSeparated: true, mantissa: 2} ).toString();
+	if (!value) return "0";
+	if (!Number(value)) return "0";
+	const discardValue = numbro(value).value().toString().match(/(\d\.)+\d{0,2}/g);
+	return numbro(discardValue[0]).format( {thousandSeparated: true, mantissa: decimals} ).toString();
 };
