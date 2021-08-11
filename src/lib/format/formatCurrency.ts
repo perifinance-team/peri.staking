3a6acd07725ad4db9a035d449e572bc9d666ac43
손손주형 <son@sonju1012.local>
2021-08-11 18:48:13 +0900

    bug fix

--- src/lib/format/formatCurrency.ts
+++ src/lib/format/formatCurrency.ts
@@ -2,6 +2,6 @@ import { utils } from 'ethers'
 export const formatCurrency = (value, decimals = 2) => {
 	if(!value) return '0';
 	const regexp = new RegExp(`(\\d+\\.)+\\d{0,${decimals}}`, 'g');
-	const cutDecimals = (utils.formatEther(value).match(regexp)[0]).toLocaleString();
+	const cutDecimals = Number(utils.formatEther(value)).toLocaleString('en', {maximumFractionDigits: decimals});
 	return cutDecimals ? cutDecimals : value.toLocaleString();
 };
