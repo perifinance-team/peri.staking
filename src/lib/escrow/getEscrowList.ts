9611372673bb3b30f9cf072167d64e71b791b141
kaen7 <kusdsuna@naver.com>
2022-06-27 12:06:54 +0900

    chore: escrow get list

--- src/lib/escrow/getEscrowList.ts
+++ src/lib/escrow/getEscrowList.ts
@@ -2,81 +2,71 @@ import { setLoading } from "config/reducers/loading";
 import { formatCurrency } from "lib/format";
 
 const daiKey =
-  "0x4441490000000000000000000000000000000000000000000000000000000000";
+	"0x4441490000000000000000000000000000000000000000000000000000000000";
 const usdcKey =
-  "0x5553444300000000000000000000000000000000000000000000000000000000";
+	"0x5553444300000000000000000000000000000000000000000000000000000000";
 
 const getCollateral = async (contracts, tempAddress, template, PeriFinance) => {
-  console.log("perifīnance", PeriFinance);
-  const pUSD =
-    BigInt(await PeriFinance.collateral(tempAddress)) -
-    BigInt(await PeriFinance.transferablePeriFinance(tempAddress));
+	const pUSD =
+		BigInt(await PeriFinance.collateral(tempAddress)) -
+		BigInt(await PeriFinance.transferablePeriFinance(tempAddress));
 
-  console.log("pUSD", pUSD);
+	template.collateral.Peri = pUSD / 10n ** 18n;
 
-  template.collateral.Peri = pUSD / 10n ** 18n;
+	const usdc = await contracts.ExternalTokenStakeManager.stakedAmountOf(
+		tempAddress,
+		usdcKey,
+		usdcKey
+	);
 
-  const usdc = await contracts.ExternalTokenStakeManager.stakedAmountOf(
-    tempAddress,
-    usdcKey,
-    usdcKey
-  );
+	template.collateral.USDC = BigInt(usdc) / 10n ** 18n;
 
-  template.collateral.USDC = BigInt(usdc) / 10n ** 18n;
+	const dai = await contracts.ExternalTokenStakeManager.stakedAmountOf(
+		tempAddress,
+		daiKey,
+		daiKey
+	);
 
-  const dai = await contracts.ExternalTokenStakeManager.stakedAmountOf(
-    tempAddress,
-    daiKey,
-    daiKey
-  );
-
-  template.collateral.Dai = BigInt(dai) / 10n ** 18n;
+	template.collateral.Dai = BigInt(dai) / 10n ** 18n;
 };
 
 export const getEscrowList = async (contracts, dispatch, PeriFinance) => {
-  dispatch(setLoading({ name: "liquidation", value: true }));
+	dispatch(setLoading({ name: "liquidation", value: true }));
 
-  console.log("dddd", PeriFinance);
+	const escrowList = [];
 
-  const escrowList = [];
+	// todo need to escrow tempAddress list
 
-  // todo need to escrow tempAddress list
+	// console.log(contracts.RewardEscrowV2.balanceOf);
 
-  console.log("PeriFinanceEscrow", await contracts.PeriFinanceEscrow);
+	const addressList = [1, 2];
 
-  // ? 보상으로 받아 Escrow 되어 있는 PERI 잔액 조회
-  // console.log(contracts.RewardEscrowV2.balanceOf);
+	for (const address of addressList) {
+		console.log(`${address}번 돌았음`);
 
-  const addressList = [1, 2];
+		let tempAddress = "0x8143BF76Bcb7e6D32E17672fAe25be38c723E286"; // todo 지울거
 
-  for (const address of addressList) {
-    console.log(`${address}번 돌았음`);
+		// todo error biding
+		const template = {
+			idx: "",
+			collateral: { Peri: 0, Dai: 0, USDC: 0 },
+			time: 0,
+		};
 
-    let tempAddress = "0x8143BF76Bcb7e6D32E17672fAe25be38c723E286"; // todo 지울거
+		try {
+			await getCollateral(contracts, tempAddress, template, PeriFinance);
+		} catch (e) {}
 
-    // todo error biding
-    const template = {
-      idx: "",
-      collateral: { Peri: 0, Dai: 0, USDC: 0 },
-      time: 0,
-    };
+		try {
+			// template.time = await PeriFinanceEscrow.TIME_INDEX();
+		} catch (e) {
+			console.log("Get Escrow time error", e);
+		}
 
-    try {
-      await getCollateral(contracts, tempAddress, template, PeriFinance);
+		await escrowList.push(template);
+	}
 
-      console.log("getCollateral", template);
-    } catch (e) {}
+	dispatch(setLoading({ name: "liquidation", value: false }));
 
-    try {
-      // template.time = await PeriFinanceEscrow.TIME_INDEX();
-    } catch (e) {
-      console.log("Get Escrow time error", e);
-    }
-
-    await escrowList.push(template);
-  }
-
-  dispatch(setLoading({ name: "liquidation", value: false }));
-
-  return escrowList;
+	return escrowList;
 };
--- src/pages/Escrow/Escrow.tsx
+++ src/pages/Escrow/Escrow.tsx
@@ -2,155 +2,155 @@ import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import { contracts } from "lib/contract";
-import { H4 } from "components/headding";
+import { H4 } from "components/heading";
 import {
-  StyledTHeader,
-  StyledTBody,
-  Row,
-  Cell,
-  BorderRow,
+	StyledTHeader,
+	StyledTBody,
+	Row,
+	Cell,
+	BorderRow,
 } from "components/Table";
 
 import { getEscrowList } from "lib/escrow";
 import { setLoading } from "config/reducers/loading";
 
 const Escrow = () => {
-  const dispatch = useDispatch();
-  const [escrowList, setEscrowList] = useState([]);
+	const dispatch = useDispatch();
+	const [escrowList, setEscrowList] = useState([]);
 
-  const { PeriFinance } = contracts as any;
+	const { PeriFinance } = contracts as any;
 
-  const getEscrowListData = useCallback(
-    async (isLoading) => {
-      dispatch(setLoading({ name: "escrow", value: isLoading }));
-      try {
-        await getEscrowList(contracts, dispatch, PeriFinance).then((data) =>
-          setEscrowList(data)
-        );
-      } catch (e) {
-        console.log("getEscrow error", e);
-      }
+	const getEscrowListData = useCallback(
+		async (isLoading) => {
+			dispatch(setLoading({ name: "escrow", value: isLoading }));
+			try {
+				await getEscrowList(contracts, dispatch, PeriFinance).then((data) =>
+					setEscrowList(data)
+				);
+			} catch (e) {
+				console.log("getEscrow error", e);
+			}
 
-      dispatch(setLoading({ name: "escrow", value: false }));
-    },
-    [dispatch]
-  );
+			dispatch(setLoading({ name: "escrow", value: false }));
+		},
+		[dispatch]
+	);
 
-  useEffect(() => {
-    (async () => {
-      return await getEscrowListData(true);
-    })();
-  }, [getEscrowListData, dispatch]);
+	useEffect(() => {
+		(async () => {
+			return await getEscrowListData(true);
+		})();
+	}, [getEscrowListData, dispatch]);
 
-  return (
-    <Container>
-      <TableContainer style={{ overflowY: "hidden", maxHeight: "70vh" }}>
-        <StyledTHeader>
-          <Row>
-            <AmountCell>
-              <H4 weigth={"b"}>Index</H4>
-            </AmountCell>
-            <AmountCell>
-              <H4 weigth={"b"}>IDX</H4>
-            </AmountCell>
-            <AmountCell>
-              <H4 weigth={"b"} style={{ width: "35rem" }}>
-                Collateral amount
-              </H4>
-            </AmountCell>
-            <AmountCell>
-              <H4 weigth={"b"}>Time</H4>
-            </AmountCell>
-          </Row>
-        </StyledTHeader>
-        <StyledTBody>
-          {escrowList.map((el, idx) => {
-            return (
-              <BorderRow
-                key={`row${idx}`}
-                style={{ minHeight: "9rem", height: "10rem" }}
-              >
-                <AmountCell>
-                  <H4 weigth={"m"}>{idx + 1}</H4>
-                </AmountCell>
-                <AmountCell>
-                  <H4 weigth={"m"}>{`oxlx${idx + 2}y`}</H4>
-                </AmountCell>
-                <AmountCell style={{ width: "35rem" }}>
-                  <CollateralList>
-                    {Object.keys(el.collateral).map((item, index) => {
-                      return (
-                        <Image key={`escrow${index}`}>
-                          <img
-                            src={`/images/currencies/${item.toUpperCase()}.png`}
-                            alt=""
-                          />
-                          <span>{`${item} ${el.collateral[item]}`}</span>
-                        </Image>
-                      );
-                    })}
-                  </CollateralList>
-                </AmountCell>
-                <AmountCell>
-                  <H4 weigth={"m"}>{el.time === 0 ? "-" : el.time}</H4>
-                </AmountCell>
-              </BorderRow>
-            );
-          })}
-        </StyledTBody>
-      </TableContainer>
-    </Container>
-  );
+	return (
+		<Container>
+			<TableContainer style={{ overflowY: "hidden", maxHeight: "70vh" }}>
+				<StyledTHeader>
+					<Row>
+						<AmountCell>
+							<H4 weight={"b"}>Index</H4>
+						</AmountCell>
+						<AmountCell>
+							<H4 weight={"b"}>IDX</H4>
+						</AmountCell>
+						<AmountCell>
+							<H4 weight={"b"} style={{ width: "35rem" }}>
+								Collateral amount
+							</H4>
+						</AmountCell>
+						<AmountCell>
+							<H4 weight={"b"}>Time</H4>
+						</AmountCell>
+					</Row>
+				</StyledTHeader>
+				<StyledTBody>
+					{escrowList.map((el, idx) => {
+						return (
+							<BorderRow
+								key={`row${idx}`}
+								style={{ minHeight: "9rem", height: "10rem" }}
+							>
+								<AmountCell>
+									<H4 weight={"m"}>{idx + 1}</H4>
+								</AmountCell>
+								<AmountCell>
+									<H4 weight={"m"}>{`oxlx${idx + 2}y`}</H4>
+								</AmountCell>
+								<AmountCell style={{ width: "35rem" }}>
+									<CollateralList>
+										{Object.keys(el.collateral).map((item, index) => {
+											return (
+												<Image key={`escrow${index}`}>
+													<img
+														src={`/images/currencies/${item.toUpperCase()}.png`}
+														alt=""
+													/>
+													<span>{`${item} ${el.collateral[item]}`}</span>
+												</Image>
+											);
+										})}
+									</CollateralList>
+								</AmountCell>
+								<AmountCell>
+									<H4 weight={"m"}>{el.time === 0 ? "-" : el.time}</H4>
+								</AmountCell>
+							</BorderRow>
+						);
+					})}
+				</StyledTBody>
+			</TableContainer>
+		</Container>
+	);
 };
 
 const AmountCell = styled(Cell)`
-  max-width: 100%;
-  display: flex;
-  justify-content: center;
+	max-width: 100%;
+	display: flex;
+	justify-content: center;
 `;
 
 const Container = styled.div`
-  display: flex;
-  flex: 1;
-  width: 100%;
-  height: 100%;
-  position: relative;
-  flex-direction: column;
-  justify-content: center;
+	display: flex;
+	flex: 1;
+	width: 100%;
+	height: 100%;
+	position: relative;
+	flex-direction: column;
+	justify-content: center;
 `;
 
 const TableContainer = styled.div`
-  z-index: 1;
-  border-radius: 25px;
-  height: 40%;
-  margin: 0 70px;
-  padding: 50px 40px;
-  background-color: ${(props) => props.theme.colors.background.panel};
-  box-shadow: ${(props) => `0px 0px 25px ${props.theme.colors.border.primary}`};
-  border: ${(props) => `2px solid ${props.theme.colors.border.primary}`};
+	z-index: 1;
+	border-radius: 25px;
+	height: 40%;
+	margin: 0 70px;
+	padding: 50px 40px;
+	background-color: ${(props) => props.theme.colors.background.panel};
+	box-shadow: ${(props) => `0px 0px 25px ${props.theme.colors.border.primary}`};
+	border: ${(props) => `2px solid ${props.theme.colors.border.primary}`};
 `;
 
 const CollateralList = styled.ul`
-  display: flex;
-  justify-content: flex-start;
-  width: 35rem;
+	display: flex;
+	justify-content: flex-start;
+	width: 35rem;
 `;
 
 const Image = styled.li`
-  display: flex;
-  align-items: center;
-  margin-right: 1.8rem;
+	display: flex;
+	align-items: center;
+	margin-right: 1.8rem;
 
-  img {
-    width: 20px;
-    height: 20px;
-    margin-right: 0.3rem;
-  }
+	img {
+		width: 20px;
+		height: 20px;
+		margin-right: 0.3rem;
+	}
 
-  span {
-    color: white;
-    font-size: 1.3rem;
-  }
+	span {
+		color: white;
+		font-size: 1.3rem;
+	}
 `;
 
 export default Escrow;
